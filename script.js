5908a95c40066190b2e8593be67516d614c3ea77
Sergey <sergeylavrenov93@gmail.com>
2021-12-05 23:03:38 +0200

    optimise: to use map for inputs

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ window.addEventListener("DOMContentLoaded", function () {
   let personsInput = document.querySelectorAll(".counter-block-input")[0],
     daysInput = document.querySelectorAll(".counter-block-input")[1],
     placeInput = document.getElementById("select"),
-    priceOutput = document.getElementById("total"),
-    //values for calculation**
-    persons = 0,
-    days = 0,
-    priceToShow = 0,
-    locationPrice = 1,
+    priceOutput = document.getElementById("total");
+
+  //values for calculation**
+  const data = {
+    persons: 0,
+    days: 0,
+    priceToShow: 0,
+    locationPrice: 1,
     // to change according to price 1 day for 1 person.
     //see html -> id="select" -> value="1" to increase costs for other places
-    servicePrice = 150;
+    servicePrice: 150,
+  };
   //**values for calculation
 
   // function to check input != 0 or ""
@@ -24,49 +27,35 @@ window.addEventListener("DOMContentLoaded", function () {
       personsInput.value == "0"
     );
   }
-
   //function to update values for calculation
-  function updateValues() {
-    days = daysInput.value;
-    persons = personsInput.value;
-    locationPrice = placeInput[placeInput.selectedIndex].value;
-    priceToShow = days * persons * servicePrice * locationPrice;
-  }
+  const updateState = (input, value) => {
+    data[value] = input.value;
+    data.priceToShow =
+      data.persons * data.days * data.locationPrice * data.servicePrice;
+  };
 
   // change event on persons field
-  personsInput.addEventListener("change", function () {
-    updateValues();
-    if (checkInput()) {
-      priceOutput.innerText = 0;
-    } else {
-      priceOutput.innerText = `${priceToShow}$`;
-    }
-    console.log(priceToShow);
-    console.log(locationPrice);
+  personsInput.addEventListener("change", () => {
+    updateState(personsInput, "persons");
+    checkInput()
+      ? (priceOutput.innerText = 0)
+      : (priceOutput.innerText = `${data.priceToShow}$`);
   });
 
   // change event on days field
-  daysInput.addEventListener("change", function () {
-    updateValues();
-    if (checkInput()) {
-      priceOutput.innerText = 0;
-    } else {
-      priceOutput.innerText = `${priceToShow}$`;
-    }
-    console.log(priceToShow);
-    console.log(locationPrice);
+  daysInput.addEventListener("change", () => {
+    updateState(daysInput, "days");
+    checkInput()
+      ? (priceOutput.innerText = 0)
+      : (priceOutput.innerText = `${data.priceToShow}$`);
   });
 
   // change event on places field
-  placeInput.addEventListener("change", function () {
-    updateValues();
-    if (checkInput()) {
-      priceOutput.innerText = 0;
-    } else {
-      priceOutput.innerText = `${priceToShow}$`;
-    }
-    console.log(priceToShow);
-    console.log(locationPrice);
+  placeInput.addEventListener("change", () => {
+    updateState(placeInput, "locationPrice");
+    checkInput()
+      ? (priceOutput.innerText = 0)
+      : (priceOutput.innerText = `${data.priceToShow}$`);
   });
   //#endregion
 });
